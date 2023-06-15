30c7f294c86f8b52ca62bfb377977138dd798652
jahnvit25 <86479676+jahnvit25@users.noreply.github.com>
2023-06-16 00:41:16 +0530

    Add files via upload

---
+++ Decryption.py
@@ -0,0 +1,27 @@
+try:
+    path = input(r"Enter path of your Image: ")
+
+    key= int(input("Enter key for encryption of Image: "))
+
+    print("The path of file: ", path)
+    print("Note: Encryption key and Decryption key must be same.")
+    print("Key for decryption: ")
+
+    fin = open(path, 'rb')
+
+    image = fin.read()
+    fin.close
+
+    image = bytearray(image)
+
+    for index, values in enumerate(image):
+       image[index] = values ^ key
+
+    fin = open(path, 'wb')
+    fin.write(image)
+    fin.close()
+    print("Decryption Done! ")
+       
+
+except Exception:
+    print("Error caught: ", Exception._name_)
